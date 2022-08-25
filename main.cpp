72198950a01b0873bc7094e54dcae38e250cc095
João Pedro Nunes <nunesjoao@discente.ufg.br>
2022-08-25 18:24:02 -0300

    função split

--- main.cpp
+++ main.cpp
@@ -5,15 +5,15 @@ using namespace std;
 
 typedef struct lista{
     boolean folha;
-    int dado[4];
-    struct lista *f[5];
+    int chaves[4];
+    struct lista *links[5];
     lista(){
         for(int i=0;i<4;i++)
             folha=false;
     }
 }lista;
 
-void split(lista v, int di);
+void split(lista *no, int ddado, int ordem);
 void insertionSort(int v[], int length);
 
 int main() {
@@ -32,8 +32,8 @@ void insertionSort(int v[], int length){
     }
 }
 
-void split(lista *v, int di){
-    int aux[5];
+void split(lista *no, int dado, int ordem){
+    /*int aux[5];
     for(int i=0;i<6;i++){
         if(i!=5)
         aux[i]=v->dado[i];
@@ -42,6 +42,28 @@ void split(lista *v, int di){
     }
     insertionSort(aux, 5);
     lista *p, *r;
-    p = (lista *) malloc(sizeof(lista));
-   
+    p = (lista *) malloc(sizeof(lista));*/
+    
+    int inicio = 0, final = (ordem - 2);
+    int medio = (inicio + final)/2;
+    int i, j;
+    lista *nova, *atual;
+    atual = no;
+    nova = (lista *) malloc(sizeof(lista));
+    for(i = 0, j = medio + 1; i <= medio, j <= ordem - 2; i++, j++){
+        nova->chaves[i] = atual->chaves[j];
+        atual->chaves[j] = -1;
+        nova->chaves[j] = -1;
+    }
+    if(dado > atual->chaves[medio]){
+        nova->chaves[i+1] = dado;
+        quicksort(nova->chaves);
+        //linkar o pai com os dois nós (atual e nova)
+        inserir(nova->chaves[0]);
+    }else{
+        atual->chaves[medio + 1] = dado;
+        quickSort(atual->chaves);
+        //linkar o pai com os dois nós (atual e nova)
+        inserir(atual->chaves[medio + 1]);
+    }
 }
