cca9f5c5606e67d634206c965a2e84186918c250
João Pedro Nunes <nunesjoao@discente.ufg.br>
2022-08-29 19:09:12 -0300

    -

--- ArvoreB.c
+++ ArvoreB.c
@@ -1,5 +1,7 @@
 //#include<bits/stdc++.h>
 #include <rpcndr.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "QuickSort.h"
 
 //using namespace std;
@@ -7,6 +9,7 @@
 typedef struct lista{
     boolean folha;
     int chaves[3];
+    struct lista *pai;
     struct lista *links[4];
     /*lista(){
         for(int i=0;i<4;i++)
@@ -16,12 +19,14 @@ typedef struct lista{
 
 void split(lista *no, int dado, int ordem);
 lista *busca(lista *raiz, int dado);
-void insertionSort(int v[], int length);
+void insereNoPai(lista *pai, int dado, int ordem);
+void insere(lista *raiz, int dado, int ordem);
+//void insertionSort(int v[], int length);
 
 int main() {
     return 0;
 }
-
+/*
 void insertionSort(int v[], int length){
     int temp, i, j;
     for(i = 1; i < length; i++){
@@ -32,7 +37,7 @@ void insertionSort(int v[], int length){
             v[j] = temp;
         }
     }
-}
+}*/
 
 void split(lista *no, int dado, int ordem){
     /*int aux[5];
@@ -48,7 +53,7 @@ void split(lista *no, int dado, int ordem){
     
     int inicio = 0, final = (ordem - 2);
     int medio = (inicio + final)/2;
-    int i, j;
+    int i, j, k = 0;
     lista *nova, *atual;
     atual = no;
     nova = (lista *) malloc(sizeof(lista));
@@ -59,14 +64,63 @@ void split(lista *no, int dado, int ordem){
     }
     if(dado > atual->chaves[medio]){
         nova->chaves[i+1] = dado;
-        quicksort(nova->chaves);
-        //linkar o pai com os dois nós (atual e nova)
-        inserir(nova->chaves[0]);
+        quicksort(nova->chaves, ordem - 1);
+        //linkar o pai com os dois nós (atual e nova)-
+        if (atual->pai == NULL)
+        {
+            lista *novaRaiz;
+            novaRaiz = (lista *)malloc(sizeof(lista));
+            novaRaiz->chaves[0] = dado;
+            novaRaiz->links[0] = atual;
+            novaRaiz->links[1] = nova;
+        }
+        else
+        {
+            inserirNoPai(atual->pai, atual->chaves[medio + 1], ordem);
+            while (atual->pai->links[k] != NULL)
+            {
+                k++;
+            }
+            atual->pai->links[k] = nova;
+        }
     }else{
         atual->chaves[medio + 1] = dado;
-        quickSort(atual->chaves);
+        quickSort(atual->chaves, ordem - 1);
         //linkar o pai com os dois nós (atual e nova)
-        inserir(atual->chaves[medio + 1]);
+        if(atual->pai == NULL){
+            lista *novaRaiz;
+            novaRaiz = (lista *)malloc(sizeof(lista));
+            novaRaiz->chaves[0] = dado;
+            novaRaiz->links[0] = atual;
+            novaRaiz->links[1] = nova;
+        }else{
+            inserirNoPai(atual->pai, atual->chaves[medio + 1], ordem);
+            while (atual->pai->links[k] != NULL){
+                k++;
+            }
+            atual->pai->links[k] = nova;
+        }
+    }
+}
+
+void insereNoPai(lista *pai, int dado, int ordem){
+    int i = 0, cheio = 0;
+    if(pai->chaves[ordem-2] != -1){
+        cheio = 1;
+    }
+    if(cheio == 1){
+        split(pai, dado, ordem);
+    }else if(cheio == 0){
+        int aux = 0;
+        while(aux == 0){
+            if(pai->chaves[i] != -1){
+                i++;
+            }else{
+                aux = 1;
+            }
+        }
+        pai->chaves[i] = dado;
+        quickSort(pai->chaves, ordem - 1);
     }
 }
 
